c5d623b2156280f96388a00b6cb9781928f1ee0a
DorLex <dorozhinskiysanya@gmail.com>
2023-09-04 22:00:39 +0300

    refact .env

--- project/configs.py
+++ project/configs.py
@@ -3,8 +3,6 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 from project.app import app
 
-app.debug = True
-
 DATABASE_USER = config('DATABASE_USER')
 DATABASE_PASSWORD = config('DATABASE_PASSWORD')
 DATABASE_HOST = config('DATABASE_HOST')
@@ -12,11 +10,12 @@ DATABASE_NAME = config('DATABASE_NAME')
 
 SQLALCHEMY_DATABASE_URI = f'postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}'
 
+app.debug = config('DEBUG', cast=bool)
+
 app.config['SECRET_KEY'] = config('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config('SQLALCHEMY_TRACK_MODIFICATIONS', cast=bool)
 
-# app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_RECORD_QUERIES'] = True
+app.config['SQLALCHEMY_RECORD_QUERIES'] = config('SQLALCHEMY_RECORD_QUERIES', cast=bool)
 
 toolbar = DebugToolbarExtension(app)
