4a88e71c14dddc6767eb6c3384eb9489ba48b5aa
DorLex <dorozhinskiysanya@gmail.com>
2025-02-13 21:11:05 +0300

    refact(controllers): refact LiveSearch controller

--- project/controllers/live_search.py
+++ project/controllers/live_search.py
@@ -6,7 +6,7 @@ from project.services import crud
 
 class LiveSearchIngredients(MethodView):
     def get(self):
-        input_ingredient = request.args.get('ingredient')
-        ingredients_list = crud.get_ingredients_ilike(input_ingredient)
+        input_ingredient: str = request.args.get('ingredient')
+        ingredients: list[str] = crud.get_ingredients_ilike(input_ingredient)
 
-        return jsonify(ingredients_list)
+        return jsonify(ingredients)
--- project/urls.py
+++ project/urls.py
@@ -6,6 +6,7 @@ from project.controllers.recipes import RecipeDetails, SearchRecipes
 
 urls = [
     app.add_url_rule('/', view_func=HomeView.as_view('home')),
+    
     app.add_url_rule('/recipe/<int:recipe_id>', view_func=RecipeDetails.as_view('recipe_details')),
     app.add_url_rule('/search-recipes-by-ingredients', view_func=SearchRecipes.as_view('search_recipes')),
     app.add_url_rule('/live-search', view_func=LiveSearchIngredients.as_view('live_search_ingredients')),
