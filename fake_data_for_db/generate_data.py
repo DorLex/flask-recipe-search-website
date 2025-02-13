df83e912e1c622d3937af5505ff80c725efef7f5
DorLex <dorozhinskiysanya@gmail.com>
2025-02-13 17:36:01 +0300

    refact(build): refact service for generate fake data to db

--- fake_data_for_db/generate_data.py
+++ fake_data_for_db/generate_data.py
@@ -1,24 +1,23 @@
-from fake_data_for_db.generate_services.create_services import create_recipe
-from fake_data_for_db.generate_services.get_services import get_or_create_ingredient
-from fake_data_for_db.generate_services.random_services import get_ingredient_num
-from project.db_models import db
+from fake_data_for_db.services.crud import create_recipe, get_or_create_ingredient
+from fake_data_for_db.services.randomizer import get_ingredient_num
+from project.db_models import db, Recipes, Ingredients
 
 
-def _save(obj):
+def _save(obj: db.Model) -> None:
     db.session.add(obj)
     db.session.flush()
 
 
-def save_fake_data_to_db():
+def save_fake_data_to_db() -> None:
     for recipes_num in range(1, 16):
-        recipe = create_recipe(recipes_num)
+        recipe: Recipes = create_recipe(recipes_num)
         _save(recipe)
 
-        used_numbers = dict()
-        for _ in range(1, 6):
-            ingredient_num = get_ingredient_num(used_numbers)
+        used_numbers = set()
+        for _ in range(5):
+            ingredient_num: int = get_ingredient_num(used_numbers)
 
-            ingredient = get_or_create_ingredient(ingredient_num)
+            ingredient: Ingredients = get_or_create_ingredient(ingredient_num)
             _save(ingredient)
 
             recipe.ingredients.append(ingredient)
