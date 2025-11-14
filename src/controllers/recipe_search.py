5b71ce19b10e4896f6ea4ed562c9b9dd63049eca
DorLex <dorozhinskiysanya@gmail.com>
2025-11-14 23:48:51 +0300

    feat: update

--- src/controllers/recipe_search.py
+++ src/controllers/recipe_search.py
@@ -8,12 +8,14 @@ from src.services.recipe import RecipeService
 
 class RecipeSearchView(MethodView):
     def get(self) -> str:
-        raw_ingredient_titles: str = request.args.get('ingredients')
+        raw_ingredient_titles: str | None = request.args.get('ingredients')
+        if not raw_ingredient_titles:
+            flash('Неверно переданы названия ингредиентов')
+            return render_template('search.html', recipes=[], title='Поиск')
 
         recipe_service: RecipeService = RecipeService(RecipeRepository())
         recipes: list[Recipe] = recipe_service.get_recipes_by_ingredients(raw_ingredient_titles)
 
         if not recipes:
             flash('НЕ НАЙДЕНО')
-
         return render_template('search.html', recipes=recipes, title='Поиск')
